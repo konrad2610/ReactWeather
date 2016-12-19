f47deeb82920564da0297534399d8f62717ada90
Konrad Beśka <beska.konrad@gmail.com>
2016-12-19 22:15:58 +0100

    Change About and Examples page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,12 @@
 var React = require('react');
 
-var About = (props) => <h3>About Component</h3>;
+var About = (props) => {
+  return (
+    <div>
+      <h3>About Component</h3>
+      <p>Welcome to about page!</p>
+    </div>
+  );
+};
 
 module.exports = About;
