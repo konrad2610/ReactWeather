743b30a98642803ec40ede2404c273385063f6b1
Konrad Beśka <beska.konrad@gmail.com>
2016-12-29 23:08:16 +0100

    Style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,20 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to about page!</p>
+      <h1 className='text-center'>About</h1>
+      <p>This is a weather application build on React.</p>
+      <p>Here are some of the tools used:</p>
+      <ul>
+        <li>
+          <a href='https://facebook.github.io/react'>React</a> - JavaScript framework.
+        </li>
+        <li>
+          <a href='http://openweathermap.org'>Open Weather Map</a> - API to search for weather data by city name.
+        </li>
+        <li>
+          <a href='https://github.com/konrad2610/ReactWeather'>Repo</a> - Project repo.
+        </li>
+      </ul>
     </div>
   );
 };
